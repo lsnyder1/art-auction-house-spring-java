32eaf41829078f59e5943f66eeadc857de3ad0f3
Lonny Snyder <lonnysnyder81@gmail.com>
2017-11-09 15:26:37 -0600

    added successful logged in view and test validation

--- src/main/java/com/example/artauctionhouse/controllers/HomeController.java
+++ src/main/java/com/example/artauctionhouse/controllers/HomeController.java
@@ -2,6 +2,8 @@ package com.example.artauctionhouse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -12,14 +14,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
 
-    @RequestMapping(value = "")
-
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index() {
         return "home/index";
     }
-    @RequestMapping(value="/login")
+    @RequestMapping(value="/login", method = RequestMethod.GET)
     public String login(){
         return"home/login";
+    }
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public String loggedin(@RequestParam String username){
+        username=username;
+        if (username!=""){
+            return"loggedin/loggedin";
+
+        }
+        else{
+            return"home/login";
+        }
     }
 }
 
